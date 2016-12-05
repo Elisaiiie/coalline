23cc6354be185ce4e9d1af5d478c4cc959a1b8eb
stephane <sk@link2b.fr>
2016-12-05 12:17:46 +0100

    Intégration des temps de passages Intégration des pages de bascule des capteurs Amélioration de la feuille de style et de l'affichage de l'état des capteurs

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
     var array = url.split('/');
     var currentUrl = array[array.length - 1];
 
+
+    // Declare sensors
+
+    $('body').prepend('<div class="sensors">' +
+        '<div class="sensor_1">0</div>' +
+        '<div class="sensor_2">0</div>' +
+        '<div class="sensor_3">0</div>' +
+        '</div>');
+
     setInterval(function () {
         getSensors(currentUrl);
     }, 500);
@@ -20,8 +29,8 @@ function getSensors(currentUrl) {
     }).done(function (json) {
         $('.sensors .sensor_' + json.sensor).text(json.current);
         if (json.current == 1) {
-            if (currentUrl != "regles.html") {
-                document.location = "regles.html";
+            if (currentUrl != "charbon.html") {
+                document.location = "charbon.html";
             }
         }
     });
@@ -30,8 +39,8 @@ function getSensors(currentUrl) {
     }).done(function (json) {
         $('.sensors .sensor_' + json.sensor).text(json.current);
         if (json.current == 1) {
-            if (currentUrl != "indication1.html") {
-                document.location = "indication1.html";
+            if (currentUrl != "video.html") {
+                document.location = "video.html";
             }
         }
     });
@@ -40,8 +49,8 @@ function getSensors(currentUrl) {
     }).done(function (json) {
         $('.sensors .sensor_' + json.sensor).text(json.current);
         if (json.current == 1) {
-            if (currentUrl != "charbon.html") {
-                document.location = "charbon.html";
+            if (currentUrl != "musiqueinterview.html") {
+                document.location = "musiqueinterview.html";
             }
         }
     });
